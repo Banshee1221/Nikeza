f5006639869716a4fab3505c4eb8e9ca62434891
Eugene de Beste <banshee1221@gmail.com>
2017-05-25 18:09:19 +0200

    quick change 2

--- Tradere/plugins/openstack_bash.py
+++ Tradere/plugins/openstack_bash.py
@@ -6,11 +6,11 @@ class Plugin:
     token = ""
 
     def __init__(self, user, passwd):
-        token_req = '{"auth": {"identity": {"methods": ["password"], "password": {"user": ' \
-                    '{"domain": {"id": "default"}, "name": "{0}", "password": "{1}"}}}}}'.format(str(user), str(passwd))
+        token_req = '{"auth":{"identity":{"methods":["password"], "password":{"user":{"domain":{"id":"default"}, "name":"{0}","password":"{1}"}}}}}'.format(
+            str(user), str(passwd))
 
-        self.token = subprocess.check_output(+
-            ["curl", "-si", "-d " + token_req + "", "-H 'Content-type: application/json'",
+        self.token = subprocess.check_output(
+            ["curl", "-si", "-d " + str(token_req) + "", "-H 'Content-type: application/json'",
              "http://localhost:35357/v3/auth/tokens | awk '/X-Subject-Token/ {print $2}'"])
         print(self.token)
 
