d8b1bdacb51407bf75155797851b0b7efb3a86e4
Eugene de Beste <banshee1221@gmail.com>
2017-05-25 17:11:44 +0200

    temp changes test

--- Tradere/plugins/openstack_bash.py
+++ Tradere/plugins/openstack_bash.py
@@ -1,19 +1,35 @@
-import os
 import subprocess
 
 
 class Plugin:
     template = ""
+    token = ""
 
     def __init__(self, user, passwd):
-        os.environ['OS_USERNAME'] = user
-        os.environ['OS_PASSWORD'] = passwd
-        os.environ['OS_PROJECT_NAME'] = user
-        os.environ['OS_USER_DOMAIN_NAME'] = "Default"
-        os.environ['OS_PROJECT_DOMAIN_NAME'] = "Default"
-        os.environ['OS_AUTH_URL'] = "http://controller.cluster:35357/v3"
-        os.environ['OS_IDENTITY_API_VERSION'] = "3"
-        os.environ['OS_IMAGE_API_VERSION'] = "2"
+        token_req = {
+            "auth": {
+                "identity": {
+                    "methods": [
+                        "password"
+                    ],
+                    "password": {
+                        "user": {
+                            "domain": {
+                                "id": "default"
+                            },
+                            "name": user,
+                            "password": passwd
+                        }
+                    }
+                }
+            }
+        }
+
+        self.token = subprocess.check_output(
+            ["curl", "-si", "-d " + str(token_req) + "", "-H 'Content-type: application/json'",
+             "http://localhost:35357/v3/auth/tokens | awk '/X-Subject-Token/ {print $2}'"])
+        print(self.token)
+
 
     def queue_list(self):
         p = subprocess.check_output(['magnum', "cluster-list"])
